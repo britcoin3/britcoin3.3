d31b625bf8f9a3be5b8e3da61089fa556d34fddc
piggygreenmo <piggygreenmo@gmail.com>
2016-11-08 05:15:31 +0000

    Minor fix to spacing

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -21,7 +21,7 @@ static std::map<int, unsigned int> mapStakeModifierCheckpoints =
         (       0, 0x0e00670bu )
         (    2750, 0xe86d786au )
         (   22750, 0x8f0d14b6u )
-        ( 977750, 0x0f7a3d59u )
+        (  977750, 0x0f7a3d59u )
         ( 1028500, 0x46b88f37u )
         ( 1050000, 0x67874a64u )
         ( 1056600, 0xe0e9b2eau )
